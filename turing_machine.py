744d235218f94b0475a26b372714a47ab0578b43
therockmandolinist <diegoamundo@protonmail.com>
2017-05-04 19:16:23 -0400

    Clarification and documentation.

--- turing_machine.py
+++ turing_machine.py
@@ -14,7 +14,7 @@ import sys
 from docopt import docopt
 
 class TuringMachine(object):
-    def __init__(self, filename, initial, rate=.05):
+    def __init__(self, filename, initial, rate):
         self.commands = self.parse_file(filename)
         self.tape = list(f"_{initial}_")
         self.rate = float(rate)
@@ -24,6 +24,7 @@ class TuringMachine(object):
         self.wild_key = (self.state, "*")
 
     def parse_file(self, filename):
+        "Parse turing code from filename into a dict of commands."
         with open(filename) as f:
             lines = map(str.strip, f.read().splitlines())
             lines = [line for line in lines if not line.startswith(';')]
@@ -31,37 +32,53 @@ class TuringMachine(object):
             return dict([(tuple(i[:2]),tuple(i[2:])) for i in lines])
 
     def run(self):
+        "Run the turing machine."
         while (self.key in self.commands
                or self.wild_key in self.commands
                and not self.key[0].startswith("halt")):
+            # Update rate
             time.sleep(self.rate)
-            newchar, action, newstate = self.commands.get(self.key) or self.commands[self.wild_key]
-            self.tape[self.place] = (newchar if newchar != '*' else self.tape[self.place])
+            # Get things to do from hash table
+            newchar, action, newstate = (self.commands.get(self.key)
+                                         or self.commands[self.wild_key])
+            self.tape[self.place] = (newchar
+                                     if newchar != '*'
+                                     else self.tape[self.place])
 
+            # Update the tape accordingly
             if action == "l":
                 self.place = self.place - 1
-                if self.place == -1:
+                if self.place == -1: # Handle moving past beginning
                     self.tape.insert(0,"_")
                     self.place = 0
             elif action == "r":
                 self.place = self.place + 1
-                if self.place == len(self.tape):
+                if self.place == len(self.tape): # Handle moving past end
                     self.tape.append("_")
+
+            # Update the current state/key/wild
             self.state = newstate
-            # print(self.state)
             self.key = (self.state, self.tape[self.place])
             self.wild_key = (self.state, '*')
+
+            # Make visualization
             tape_viz = copy.deepcopy(self.tape)
             tape_viz[self.place] = f'\033[7m{tape_viz[self.place]}\033[0m'
-            print(''.join(tape_viz), end=('\r' if not self.key[0].startswith("halt") else ' '))
+            print(''.join(tape_viz), end=('\r'
+                                          if not self.key[0].startswith("halt")
+                                          else ' '))
         else:
             if not self.key[0].startswith("halt"):
-                print(f"No rule for state '{self.state}' and char '{self.tape[self.place]}'.")
+                print(f"No rule for state '{self.state}' "
+                      f"and char '{self.tape[self.place]}'.")
             else:
                 print('...Done!')
 
 if __name__ == "__main__":
     arguments = docopt(__doc__)
     # print(arguments)
-    machine = TuringMachine(arguments['FILENAME'], arguments['INITIAL'], (arguments['RATE'] or .05))
+    machine = TuringMachine(
+        arguments['FILENAME'],
+        arguments['INITIAL'],
+        (arguments['RATE'] or .05))
     machine.run()
